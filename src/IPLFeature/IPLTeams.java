13f2178a08630b2fcb392c1787a1eabfe9a03ba3
LogeshSDR <krishhlogesh@gmail.com>
2022-03-26 21:54:12 +0530

    My First Project

---
+++ src/IPLFeature/IPLTeams.java
@@ -0,0 +1,27 @@
+package IPLFeature;
+
+public class IPLTeams {
+	
+	public void csk() {
+		System.out.println("Chennai Super kings");
+	}
+	
+	public void Teams() {
+		System.out.println("DHONI");
+	}
+	
+	public void runs() {
+		System.out.println("50");
+	}
+	
+
+	public static void main(String[] args) {
+		IPLTeams t = new IPLTeams();
+		t.csk();
+		t.Teams();
+		t.runs();
+		
+		
+		
+	}
+}
